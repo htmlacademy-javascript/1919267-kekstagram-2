222599c5b47bb4047980aa754a821982ea50c953
Tatyana Nemchina <23angorka07@gmail.com>
2024-10-31 17:40:26 +0300

    добавляет открытие и закрытие формы загрузки фото

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import './thumbnails.js';
+import './upload-form.js';
 import {generatePhotos} from './data.js';
 import {PHOTOS_COUNT} from './const.js';
 import { initThumbnails } from './thumbnails.js';
